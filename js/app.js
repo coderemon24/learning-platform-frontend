b883cda01f8fc12de19c43f7daa5fafc4a6697b4
emon2021 <mdemonahmed2021@gmail.com>
2025-01-02 22:56:58 +0600

    added mixitup plugin to active courses section

--- js/app.js
+++ js/app.js
@@ -1,11 +1,30 @@
-
-//  shrink navbar on scroll
-$(window).scroll(function(){
+$(document).ready(function (){
+  
+  //  shrink navbar on scroll
+  $(window).scroll(function(){
     var wscroll = $(this).scrollTop();
     if(wscroll > 50){
-     $(".navbar").addClass("shrink-nav");
+    $(".navbar").addClass("shrink-nav");
     }
     else{
       $(".navbar").removeClass("shrink-nav");
     }
+  });
+
+  //  mixitup 
+  let mixer = mixitup('.box_list', {
+  selectors: {
+      target: '.mix',
+      control: '.box_control'
+  },
+  animation: {
+      duration: 300
+  }
+  });
+
+  // Set default active control dynamically
+  $('.box_control').removeClass('mixitup-control-active'); // Remove active from all
+  $('[data-filter="*"]').addClass('mixitup-control-active'); // Add active to default
+  
+  
 });
